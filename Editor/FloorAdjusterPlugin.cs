5eb0aed0cb6e2234ab066e637dba8a77146c6f48
Narazaka <info@narazaka.net>
2023-11-09 12:23:50 +0900

    MA関係ない

--- Editor/FloorAdjusterPlugin.cs
+++ Editor/FloorAdjusterPlugin.cs
@@ -1,9 +1,5 @@
 ﻿using nadena.dev.ndmf;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using VRC.SDK3.Dynamics.PhysBone.Components;
-using nadena.dev.modular_avatar.core;
 using VRC.SDK3.Avatars.Components;
 
 [assembly: ExportsPlugin(typeof(Narazaka.VRChat.FloorAdjuster.Editor.FloorAdjusterPlugin))]
@@ -20,30 +16,31 @@ namespace Narazaka.VRChat.FloorAdjuster.Editor
         public override string DisplayName => nameof(FloorAdjuster);
         protected override void Configure()
         {
-            InPhase(BuildPhase.Resolving).BeforePlugin("nadena.dev.modular-avatar").Run(nameof(FloorAdjuster), ctx =>
+            InPhase(BuildPhase.Transforming).AfterPlugin("nadena.dev.modular-avatar").Run(nameof(FloorAdjuster), ctx =>
             {
                 var floorAdjuster = ctx.AvatarRootObject.GetComponentInChildren<FloorAdjuster>();
                 if (floorAdjuster == null) return;
 
-                var mergeArmatures = ctx.AvatarRootObject.GetComponentsInChildren<ModularAvatarMergeArmature>();
-                var descriptor = ctx.AvatarRootObject.GetComponentInChildren<VRCAvatarDescriptor>();
+                AdjustArmature(floorAdjuster);
 
+                var descriptor = ctx.AvatarRootObject.GetComponentInChildren<VRCAvatarDescriptor>();
                 descriptor.ViewPosition += Vector3.up * floorAdjuster.Height;
 
-                AdjustArmature(floorAdjuster.transform, floorAdjuster.Height);
-                foreach (var mergeArmature in mergeArmatures)
-                {
-                    AdjustArmature(mergeArmature.transform, floorAdjuster.Height);
-                }
+                UnityEngine.Object.DestroyImmediate(floorAdjuster);
             });
         }
 
-        void AdjustArmature(Transform armature, float height)
+        void AdjustArmature(FloorAdjuster floorAdjuster)
         {
+            var armature = floorAdjuster.transform;
             var hips = armature.Find("Hips");
-            var armatureScale = 1 + height / (hips.transform.position.y - armature.transform.position.y);
+            var armaturePosition = armature.position;
+            var hipsPosition = hips.position;
+            var armatureScale = 1 + floorAdjuster.Height / (hipsPosition.y - armaturePosition.y);
+            var scaledZdiff = (hipsPosition.z - armaturePosition.z) * (armatureScale - 1);
             armature.localScale = Vector3.one * armatureScale;
             hips.localScale = Vector3.one / armatureScale;
+            armature.position -= scaledZdiff * Vector3.forward;
         }
     }
 }
